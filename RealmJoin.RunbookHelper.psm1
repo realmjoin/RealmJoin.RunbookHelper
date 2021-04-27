bf185f7b3a3f4d9728f107a4c8d5fe48a54f14fb
Jan Berdel <jan.berdel@glueckkanja-gab.com>
2021-04-27 15:21:43 +0200

    v0.1.0-test

---
+++ RealmJoin.RunbookHelper.psm1
@@ -0,0 +1,37 @@
+﻿#requires -Version 5.0
+
+class RealmJoinUIAttribute : Attribute
+{
+    [Entity]$Graph = [Entity]::None;
+    [Picker]$Picker = [Picker]::None;
+    [string]$Attribute;
+    [string]$Filter;
+
+    RealmJoinUIAttribute([Entity]$Graph, [string]$Attribute, [string]$Filter)
+    {
+        $this.Graph = $Graph;
+        $this.Attribute = $Attribute;
+        $this.Filter = $Filter;
+    }
+
+    RealmJoinUIAttribute([Picker]$Picker)
+    {
+        $this.Picker = $Picker;
+    }
+}
+
+enum Entity
+{
+    None = 0
+    User = 10
+    Group = 20
+    Device = 30
+}
+
+enum Picker
+{
+    None = 0
+    Date = 10
+    Time = 20
+    DateTime = 30
+}
