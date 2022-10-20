3510b4016e82a2ad2e272796254db4560a577408
Hans-Carl Oberdalhoff <Hans-Carl.Oberdalhoff@glueckkanja-gab.com>
2022-10-20 11:46:14 +0200

    Support ManagedId with ExchangeOnline (req. v3 of EXO module)

--- ConnectionExchangeOnline.ps1
+++ ConnectionExchangeOnline.ps1
@@ -1,22 +1,40 @@
 function Connect-RjRbExchangeOnline {
     [CmdletBinding()]
     param (
-        [string] $AutomationConnectionName = "AzureRunAsConnection"
+        [string] $AutomationConnectionName = "AzureRunAsConnection",
+        [bool] $tryToUseManagedId = $true
     )
 
     # see RealmJoin.RunbookHelper.psm1
     $Global:VerbosePreference = "SilentlyContinue"
 
-    $autoCon = getAutomationConnectionOrFromLocalCertificate $AutomationConnectionName
-
-    if ($autoCon.TenantId -inotlike "*.onmicrosoft.com") {
-        Write-RjRbLog "Trying to determine initial domain name (*.onmicrosoft.com) for tenant Guid '$($autoCon.TenantId)' using Graph"
+    $signedIn = $false
+    if ($tryToUseManagedId) {
+        # Requires v3 of ExchangeOnline Module
         Connect-RjRbGraph
-        $autoCon.TenantId = Invoke-RjRbRestMethodGraph /organization | Select-Object -ExpandProperty verifiedDomains | `
-            Where-Object { $_.isInitial } | Select-Object -First 1 -ExpandProperty name
+        $tenantId = Invoke-RjRbRestMethodGraph /organization | Select-Object -ExpandProperty verifiedDomains | `
+        Where-Object { $_.isInitial } | Select-Object -First 1 -ExpandProperty name
+        try {
+            Connect-ExchangeOnline -Organization $tenantId -ManagedIdentity
+            Write-RjRbLog "Connecting to ExchangeOnline via ManagedIdentity succeded."
+            $signedIn = $true
+        } catch {
+            Write-RjRbLog "Connecting to ExchangeOnline via ManagedIdentity failed."
+        }
     }
+    
+    if (-not $signedIn) {
+        $autoCon = getAutomationConnectionOrFromLocalCertificate $AutomationConnectionName
 
-    Write-RjRbLog "Connecting with ExchangeOnline module" $autoCon
-    Connect-ExchangeOnline -Organization $autoCon.TenantId -AppId $autoCon.ApplicationId `
-        -CertificateThumbprint $autoCon.CertificateThumbprint -ShowBanner:$false
+        if ($autoCon.TenantId -inotlike "*.onmicrosoft.com") {
+            Write-RjRbLog "Trying to determine initial domain name (*.onmicrosoft.com) for tenant Guid '$($autoCon.TenantId)' using Graph"
+            Connect-RjRbGraph
+            $autoCon.TenantId = Invoke-RjRbRestMethodGraph /organization | Select-Object -ExpandProperty verifiedDomains | `
+                Where-Object { $_.isInitial } | Select-Object -First 1 -ExpandProperty name
+        }
+
+        Write-RjRbLog "Connecting with ExchangeOnline module" $autoCon
+        Connect-ExchangeOnline -Organization $autoCon.TenantId -AppId $autoCon.ApplicationId `
+            -CertificateThumbprint $autoCon.CertificateThumbprint -ShowBanner:$false
+    }
 }
